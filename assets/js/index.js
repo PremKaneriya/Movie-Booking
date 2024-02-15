042251d7c38527eea5221b560de0015e7974b7e1
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-02-15 11:19:06 +0530

    Done ClassWork and HomeWork

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,27 @@ const cinema = async () => {
     let print = '';
 
     data.map((v) => {
-        print += `<div class="cinemaCard">
+        print += `<div id="cinemaCard">
             <div>${v.name}</div>
             <img id="cinemaImage" src="${v.image}" alt="${v.name}"/>
             <p>${v.address}</p>
             <p>${v.phoneNumber}</p>
             <p>${v.email}</p>
             <p>${v.status}</p>
+            <p>${v.id}</p>
+            <button onclick="handleOpenCinema('${v.id}')">Open</button>
         </div>`;
-    });
 
+    });
     document.getElementById("cinemaContainer").innerHTML = print;
+}
+
+const handleOpenCinema = (id) => {
+    console.log("id: ", id);
+    
+    const saveId = localStorage.setItem("selectedCinema", id);
+
+    window.location.href = '/userSide/cinemaOpen.html';
 }
 
 const movie = async () => {
@@ -27,13 +37,14 @@ const movie = async () => {
     let print = '';
     data.forEach((v) => {
         if (!uniqueMovieNames.has(v.name)) {
-            print += `<div class="movieCard">
-               <div>${v.name}</div>
-               <img id="movieImage" src="${v.image}" alt="${v.name}"/>
-               <p>${v.duration}</p>
-               <p>${v.description}</p>
-               <p>${v.status}</p>
-           </div>`;
+            print += `<div id="movieCard">
+                   <div>${v.name}</div>
+                   <img id="movieImage" src="${v.image}" alt="${v.name}"/>
+                   <p>${v.duration}</p>
+                   <p>${v.description}</p>
+                   <p>${v.status}</p>
+                   <p>${v.cinemaId}</p>
+               </div>`;
             uniqueMovieNames.add(v.name);
         }
     });
